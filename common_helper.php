279be524867578269d487e6937c0aab68013371c
imchaitanyadesai <35953844+imchaitanyadesai@users.noreply.github.com>
2018-05-09 15:34:27 +0530

    Update common_helper.php

--- common_helper.php
+++ common_helper.php
@@ -323,5 +323,19 @@ function ratingStar($ratingStar){
         }
     }
 }
+function unique_multidim_array($array, $key) {
+		$temp_array = array();
+		$i = 0;
+		$key_array = array();
+
+		foreach($array as $val) {
+			if (!in_array($val[$key], $key_array)) {
+				$key_array[$i] = $val[$key];
+				$temp_array[] = $val;
+			}
+			$i++;
+		}
+		return $temp_array;
+    }
 
 ?>
