0c8f5f253c1db551d21717ec5642ed202b15822f
Chaitanya Desai <35953844+imchaitanyadesai@users.noreply.github.com>
2018-06-26 17:50:04 +0530

    Update removeIndex.php

--- removeIndex.php
+++ removeIndex.php
@@ -1,9 +1,16 @@
 //For wamp
 
 RewriteEngine On
-RewriteBase /pg/YourLifeRockApp/
+RewriteBase /CodigniterProject/
 RewriteCond %{REQUEST_URI} ^system.*
 RewriteRule ^(.*)$ /index.php/$1 [L]
 RewriteCond %{REQUEST_FILENAME} !-f
 RewriteCond %{REQUEST_FILENAME} !-d
 RewriteRule ^(.*)$ index.php/$1 [L]
+
+//For xampp
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^(.*)$ index.php/$1 [L] 
+
